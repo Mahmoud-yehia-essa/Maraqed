3514c7d1eb810afe03f354cc5a40be1373818427
Mahmoud Yehia Essa <essa2002135@gmail.com>
2025-05-25 05:55:43 +0300

    update title

--- resources/views/admin/master_admin.blade.php
+++ resources/views/admin/master_admin.blade.php
@@ -42,7 +42,7 @@
 
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
 
-	<title>لوحة تحكم - تطبيق چريمبة</title>
+	<title>لوحة تحكم - تطبيق مراقد</title>
     <style>
 
 		body
