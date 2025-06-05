ebfdd72cbd10d3cca0e7a896be9732a88601a8df
Mahmoud Yehia Essa <essa2002135@gmail.com>
2025-06-05 23:39:12 +0300

    add birthDate

--- resources/views/admin/master_admin.blade.php
+++ resources/views/admin/master_admin.blade.php
@@ -42,10 +42,7 @@
 
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
 
-<<<<<<< HEAD
-=======
 	<title>لوحة تحكم وادارة - تطبيق مراقد</title>
->>>>>>> c494b915 (change title)
     <style>
 
 		body
